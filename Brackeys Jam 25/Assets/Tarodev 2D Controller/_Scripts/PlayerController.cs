6bcb8d073eb71630686a45792e827940384b87bc
ayesha-099 <Ayeshaarooj620@gmail.com>
2025-02-20 15:44:08 +0500

    SET

--- Brackeys Jam 25/Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
+++ Brackeys Jam 25/Assets/Tarodev 2D Controller/_Scripts/PlayerController.cs
@@ -1,228 +1,17 @@
-//using System;
-//using UnityEngine;
-
-//namespace TarodevController
-//{
-//    /// <summary>
-//    /// Hey!
-//    /// Tarodev here. I built this controller as there was a severe lack of quality & free 2D controllers out there.
-//    /// I have a premium version on Patreon, which has every feature you'd expect from a polished controller. Link: https://www.patreon.com/tarodev
-//    /// You can play and compete for best times here: https://tarodev.itch.io/extended-ultimate-2d-controller
-//    /// If you hve any questions or would like to brag about your score, come to discord: https://discord.gg/tarodev
-//    /// </summary>
-//    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
-//    public class PlayerController : MonoBehaviour, IPlayerController
-//    {
-//        [SerializeField] private ScriptableStats _stats;
-//        private Rigidbody2D _rb;
-//        private CapsuleCollider2D _col;
-//        private FrameInput _frameInput;
-//        private Vector2 _frameVelocity;
-//        private bool _cachedQueryStartInColliders;
-
-//        #region Interface
-
-//        public Vector2 FrameInput => _frameInput.Move;
-//        public event Action<bool, float> GroundedChanged;
-//        public event Action Jumped;
-
-//        #endregion
-
-//        private float _time;
-
-//        private void Awake()
-//        {
-//            _rb = GetComponent<Rigidbody2D>();
-//            _col = GetComponent<CapsuleCollider2D>();
-
-//            _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
-//        }
-
-//        private void Update()
-//        {
-//            _time += Time.deltaTime;
-//            GatherInput();
-//        }
-
-//        private void GatherInput()
-//        {
-//            _frameInput = new FrameInput
-//            {
-//                JumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
-//                JumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
-//                Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
-//            };
-
-//            if (_stats.SnapInput)
-//            {
-//                _frameInput.Move.x = Mathf.Abs(_frameInput.Move.x) < _stats.HorizontalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.x);
-//                _frameInput.Move.y = Mathf.Abs(_frameInput.Move.y) < _stats.VerticalDeadZoneThreshold ? 0 : Mathf.Sign(_frameInput.Move.y);
-//            }
-
-//            if (_frameInput.JumpDown)
-//            {
-//                _jumpToConsume = true;
-//                _timeJumpWasPressed = _time;
-//            }
-//        }
-
-//        private void FixedUpdate()
-//        {
-//            CheckCollisions();
-
-//            HandleJump();
-//            HandleDirection();
-//            HandleGravity();
-
-//            ApplyMovement();
-//        }
-
-//        #region Collisions
-
-//        private float _frameLeftGrounded = float.MinValue;
-//        private bool _grounded;
-
-//        private void CheckCollisions()
-//        {
-//            Physics2D.queriesStartInColliders = false;
-
-//            // Ground and Ceiling
-//            bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
-//            bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
-
-//            // Hit a Ceiling
-//            if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
-
-//            // Landed on the Ground
-//            if (!_grounded && groundHit)
-//            {
-//                _grounded = true;
-//                _coyoteUsable = true;
-//                _bufferedJumpUsable = true;
-//                _endedJumpEarly = false;
-//                GroundedChanged?.Invoke(true, Mathf.Abs(_frameVelocity.y));
-//            }
-//            // Left the Ground
-//            else if (_grounded && !groundHit)
-//            {
-//                _grounded = false;
-//                _frameLeftGrounded = _time;
-//                GroundedChanged?.Invoke(false, 0);
-//            }
-
-//            Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
-//        }
-
-//        #endregion
-
-
-//        #region Jumping
-
-//        private bool _jumpToConsume;
-//        private bool _bufferedJumpUsable;
-//        private bool _endedJumpEarly;
-//        private bool _coyoteUsable;
-//        private float _timeJumpWasPressed;
-
-//        private bool HasBufferedJump => _bufferedJumpUsable && _time < _timeJumpWasPressed + _stats.JumpBuffer;
-//        private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _stats.CoyoteTime;
-
-//        private void HandleJump()
-//        {
-//            if (!_endedJumpEarly && !_grounded && !_frameInput.JumpHeld && _rb.velocity.y > 0) _endedJumpEarly = true;
-
-//            if (!_jumpToConsume && !HasBufferedJump) return;
-
-//            if (_grounded || CanUseCoyote) ExecuteJump();
-
-//            _jumpToConsume = false;
-//        }
-
-//        private void ExecuteJump()
-//        {
-//            _endedJumpEarly = false;
-//            _timeJumpWasPressed = 0;
-//            _bufferedJumpUsable = false;
-//            _coyoteUsable = false;
-//            _frameVelocity.y = _stats.JumpPower;
-//            Jumped?.Invoke();
-//        }
-
-//        #endregion
-
-//        #region Horizontal
-
-//        private void HandleDirection()
-//        {
-//            if (_frameInput.Move.x == 0)
-//            {
-//                var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
-//                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
-//            }
-//            else
-//            {
-//                _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
-//            }
-//        }
-
-//        #endregion
-
-//        #region Gravity
-
-//        private void HandleGravity()
-//        {
-//            if (_grounded && _frameVelocity.y <= 0f)
-//            {
-//                _frameVelocity.y = _stats.GroundingForce;
-//            }
-//            else
-//            {
-//                var inAirGravity = _stats.FallAcceleration;
-//                if (_endedJumpEarly && _frameVelocity.y > 0) inAirGravity *= _stats.JumpEndEarlyGravityModifier;
-//                _frameVelocity.y = Mathf.MoveTowards(_frameVelocity.y, -_stats.MaxFallSpeed, inAirGravity * Time.fixedDeltaTime);
-//            }
-//        }
-
-//        #endregion
-
-//        private void ApplyMovement() => _rb.velocity = _frameVelocity;
-
-//#if UNITY_EDITOR
-//        private void OnValidate()
-//        {
-//            if (_stats == null) Debug.LogWarning("Please assign a ScriptableStats asset to the Player Controller's Stats slot", this);
-//        }
-//#endif
-//    }
-
-//    public struct FrameInput
-//    {
-//        public bool JumpDown;
-//        public bool JumpHeld;
-//        public Vector2 Move;
-//    }
-
-//    public interface IPlayerController
-//    {
-//        public event Action<bool, float> GroundedChanged;
-
-//        public event Action Jumped;
-//        public Vector2 FrameInput { get; }
-//    }
-//}
 using System;
 using UnityEngine;
 
 namespace TarodevController
 {
-    [RequireComponent(typeof(Rigidbody), typeof(Collider))]
+   
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
     public class PlayerController : MonoBehaviour, IPlayerController
     {
         [SerializeField] private ScriptableStats _stats;
-        private Rigidbody _rb;
-        private CapsuleCollider _col;
+        private Rigidbody2D _rb;
+        private CapsuleCollider2D _col;
         private FrameInput _frameInput;
-        private Vector3 _frameVelocity;
+        private Vector2 _frameVelocity;
         private bool _cachedQueryStartInColliders;
 
         #region Interface
@@ -237,8 +26,10 @@ namespace TarodevController
 
         private void Awake()
         {
-            _rb = GetComponent<Rigidbody>();
-            _col = GetComponent<CapsuleCollider>();
+            _rb = GetComponent<Rigidbody2D>();
+            _col = GetComponent<CapsuleCollider2D>();
+
+            _cachedQueryStartInColliders = Physics2D.queriesStartInColliders;
         }
 
         private void Update()
@@ -287,9 +78,11 @@ namespace TarodevController
 
         private void CheckCollisions()
         {
-            // 3D collision detection using CapsuleCast
-            bool groundHit = Physics.CapsuleCast(_col.bounds.center, _col.bounds.center + Vector3.down * _col.height / 2, _col.radius, Vector3.down, out RaycastHit hit, _stats.GrounderDistance, ~_stats.PlayerLayer);
-            bool ceilingHit = Physics.CapsuleCast(_col.bounds.center, _col.bounds.center + Vector3.up * _col.height / 2, _col.radius, Vector3.up, out hit, _stats.GrounderDistance, ~_stats.PlayerLayer);
+            Physics2D.queriesStartInColliders = false;
+
+            // Ground and Ceiling
+            bool groundHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.down, _stats.GrounderDistance, ~_stats.PlayerLayer);
+            bool ceilingHit = Physics2D.CapsuleCast(_col.bounds.center, _col.size, _col.direction, 0, Vector2.up, _stats.GrounderDistance, ~_stats.PlayerLayer);
 
             // Hit a Ceiling
             if (ceilingHit) _frameVelocity.y = Mathf.Min(0, _frameVelocity.y);
@@ -310,9 +103,12 @@ namespace TarodevController
                 _frameLeftGrounded = _time;
                 GroundedChanged?.Invoke(false, 0);
             }
+
+            Physics2D.queriesStartInColliders = _cachedQueryStartInColliders;
         }
 
         #endregion
+
 
         #region Jumping
 
@@ -349,9 +145,9 @@ namespace TarodevController
         #endregion
 
         #region Horizontal
+
         private void HandleDirection()
         {
-            // Restrict movement to the X-axis only
             if (_frameInput.Move.x == 0)
             {
                 var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
@@ -361,28 +157,7 @@ namespace TarodevController
             {
                 _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
             }
-
-            // Ensure Z movement is always zero
-            _frameVelocity.z = 0;
         }
-
-        // Apply movement with velocity restricted to the X-axis
-        private void ApplyMovement()
-        {
-            _rb.velocity = new Vector3(_frameVelocity.x, _frameVelocity.y, 0);
-        }
-        //private void HandleDirection()
-        //{
-        //    if (_frameInput.Move.x == 0)
-        //    {
-        //        var deceleration = _grounded ? _stats.GroundDeceleration : _stats.AirDeceleration;
-        //        _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, 0, deceleration * Time.fixedDeltaTime);
-        //    }
-        //    else
-        //    {
-        //        _frameVelocity.x = Mathf.MoveTowards(_frameVelocity.x, _frameInput.Move.x * _stats.MaxSpeed, _stats.Acceleration * Time.fixedDeltaTime);
-        //    }
-        //}
 
         #endregion
 
@@ -404,7 +179,7 @@ namespace TarodevController
 
         #endregion
 
-        //private void ApplyMovement() => _rb.velocity = _frameVelocity;
+        private void ApplyMovement() => _rb.velocity = _frameVelocity;
 
 #if UNITY_EDITOR
         private void OnValidate()
@@ -424,8 +199,8 @@ namespace TarodevController
     public interface IPlayerController
     {
         public event Action<bool, float> GroundedChanged;
+
         public event Action Jumped;
         public Vector2 FrameInput { get; }
     }
 }
-
